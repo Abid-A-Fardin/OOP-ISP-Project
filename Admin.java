632e2a49566dae463250a5c8220c6f38a8aca085
puyaya <purnata2507@gmail.com>
2025-08-16 14:12:12 +0600

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,14 @@
+package com.example.oopispproject;
+
+import java.time.LocalDate;
+
+public class Admin extends User{
+
+
+    public Admin(String userID, String userPassword, String userType) {
+        super(userID, userPassword, userType);
+    }
+
+
+
+}
---
+++ AdminAreaManagementController.java
@@ -0,0 +1,26 @@
+package com.example.oopispproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AdminAreaManagementController {
+    @javafx.fxml.FXML
+    private ComboBox areaComboBox;
+    @javafx.fxml.FXML
+    private TextField postalCodeTextField;
+    @javafx.fxml.FXML
+    private TextField roadNumberTextField;
+    @javafx.fxml.FXML
+    private ComboBox divisionComboBox;
+    @javafx.fxml.FXML
+    private ComboBox internetPlanIdComboBox;
+
+    @javafx.fxml.FXML
+    public void saveNewAreaButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void showAllAreasButton(ActionEvent actionEvent) {
+    }
+}
